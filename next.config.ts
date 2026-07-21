d0063fa000cfe6a488758da839998c92e60a28df
anhtuanMDev <anhtuan03.mdev@gmail.com>
2026-07-21 15:18:10 +0700

    chore: update dependencies, refine tsconfig settings, and clean up next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
     images: {
         unoptimized: true,
     },
-    trailingSlash: true, // still useful for GitHub Pages
+    trailingSlash: true,
 };
 
 export default nextConfig;
